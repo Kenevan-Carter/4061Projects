24c2898a3113c3b23ee06e0d90bb483d440e233b
Kenevan-Carter <cart0660@umn.edu>
2026-04-08 14:36:51 -0500

    consumer

--- PA3_students_0326/src/consumer.c
+++ PA3_students_0326/src/consumer.c
@@ -15,10 +15,56 @@ void* consumer(void* arg) {
     */
 
     // --- YOUR CODE STARTS HERE ---
+    (void)arg;
+
+    while (1) {
+        InferenceTask task;
+
+        // Take one task from the shared queue
+        pthread_mutex_lock( &mutex);
+        while (count == 0) {
+            pthread_cond_wait(&not_empty,&mutex);
+        }
+
+
+
+
+
+        task = queue[head];
+        head = (head + 1) %BUFFER_SIZE;
+        count--;
+        // Wake producer if it was waiting for space
+        pthread_cond_signal(&not_full);
+        pthread_mutex_unlock(&mutex);
+
+        // kill when termination task is run
+        if (task.is_last) {
+            break;
+        }
+
+        //matrix vector multiplication
+        float output[NUM_CLASSES];
+
+        for (int j = 0; j < NUM_CLASSES; j++) {
+            output[j] = 0.0f;
+            for (int k = 0; k < IMG_SIZE; k++) {
+                output[j] += task.pixels[k] * weights[k][j];
+            }
+        }
+
+        //find the class with the highest score.
+        int best_class = 0;
+        for (int j = 1; j < NUM_CLASSES; j++) {
+            if (output[j] > output[best_class]) {
+                best_class = j;
+            }
+        }
+
+        //store the class prediction
+        predictions[task.image_id] = best_class;
+    }
 
 
     // --- YOUR CODE ENDS HERE ---
-
-
     return NULL;
 }
